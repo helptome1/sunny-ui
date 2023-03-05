dd239a052514ff6c511752ba0f1a2696143711b4
hezg <hzgdeemail@163.com>
2023-03-05 20:42:45 +0800

    add vite config

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,9 +1,53 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import DefineOptions from "unplugin-vue-define-options/vite";
+import dts from 'vite-plugin-dts'
+import DefineOptions from 'unplugin-vue-define-options/vite'
 export default defineConfig({
+  build: {
+    // 打包后文件目录
+    outDir: 'sunny',
+    // 压缩
+    // minify: false,
+    rollupOptions: {
+      //忽略打包vue文件
+      external: ['vue'],
+      input: ['index.ts'],
+      output: [
+        {
+          format: 'es',
+          //打包后文件名
+          entryFileNames: '[name].mjs',
+          //让打包目录和我们目录对应
+          preserveModules: true,
+          exports: 'named',
+          //配置打包根目录
+          dir: '../sunny-ui/es'
+        },
+        {
+          // 打包格式
+          format: 'cjs',
+          //打包后文件名
+          entryFileNames: '[name].js',
+          //让打包目录和我们目录对应
+          preserveModules: true,
+          exports: 'named',
+          //配置打包根目录
+          dir: '../sunny-ui/es'
+        }
+      ]
+    },
+    lib: {
+      entry: './index.ts'
+    }
+  },
   plugins: [
-    vue()
-    // DefineOptions()
+    vue(),
+    dts({
+      entryRoot: './src',
+      outputDir: ['../sunny-ui/es/src', '../sunny-ui/lib/src'],
+      //指定使用的tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
+      tsConfigFilePath: '../../tsconfig.json'
+    }),
+    DefineOptions()
   ]
 })
