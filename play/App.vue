560546f70e9cf789863896ca7379359c4f6c5019
hezg <hzgdeemail@163.com>
2023-03-05 15:51:13 +0800

    add button type for text dashed

--- play/App.vue
+++ play/App.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- <sun-button>全局引入</sun-button> -->
-  <Button type="primary">自定义引入</Button>
+  <Button type="dashed">Dashed Button</Button>
+  <Button type="text">Text Button</Button>
 </template>
 
 <script setup lang="ts">
